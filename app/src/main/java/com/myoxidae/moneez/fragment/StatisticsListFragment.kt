5f97d102d65ad210b7c3ee2bf7ad4ad0c2e274ee
niC00L <nika.blanarova@gmail.com>
2019-05-23 20:21:54 +0200

    Account picker for statistics

--- app/src/main/java/com/myoxidae/moneez/fragment/StatisticsListFragment.kt
+++ app/src/main/java/com/myoxidae/moneez/fragment/StatisticsListFragment.kt
@@ -8,20 +8,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.res.TypedArrayUtils.getDrawable
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.mynameismidori.currencypicker.ExtendedCurrency
 import com.myoxidae.moneez.*
 import com.myoxidae.moneez.model.*
+import com.myoxidae.moneez.picker.accountpicker.AccountPicker
+import kotlinx.android.synthetic.main.activity_add_transaction.*
 import kotlinx.android.synthetic.main.fragment_account.*
 import kotlinx.android.synthetic.main.fragment_account_list.*
 import kotlinx.android.synthetic.main.fragment_stat_category_list.*
+import kotlinx.android.synthetic.main.fragment_stat_category_list.view.*
 import kotlinx.android.synthetic.main.fragment_transaction_list.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class StatisticsListFragment : androidx.fragment.app.Fragment() {
+    private var accountListViewModel: AccountListViewModel? = null
 
     // TODO: Customize parameters
     private var columnCount = 1
@@ -32,6 +38,7 @@ class StatisticsListFragment : androidx.fragment.app.Fragment() {
         // TODO: Customize parameter argument names
         @JvmField
         var ARG_COLUMN_COUNT = "column-count"
+
         // TODO: Customize parameter initialization
         @JvmStatic
         fun newInstance(columnCount: Int, accountId: Long) =
@@ -48,12 +55,34 @@ class StatisticsListFragment : androidx.fragment.app.Fragment() {
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
+
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_stat_category_list, container, false)
+
+        accountListViewModel = ViewModelProviders.of(this).get(AccountListViewModel::class.java)
+        val accountList = accountListViewModel?.getAccountsList()!!.toCollection(ArrayList())
+
+        view.button_account.setOnClickListener {
+            val picker = AccountPicker.newInstance("Select Account", accountList)  // dialog title
+            picker.setListener { account ->
+                viewModel.accountId = account.accountId
+                initRecyclerView()
+                button_account?.text = account.name
+                val currency = ExtendedCurrency.getCurrencyByName(account.currency)
+                button_account?.setCompoundDrawablesWithIntrinsicBounds(
+                    context?.getDrawable(currency.flag),
+                    null, null, null
+                )
+                picker.dismiss()
+            }
+            picker.show(activity!!.supportFragmentManager, "ACCOUNT_PICKER")
+        }
 
         // Set the adapter
         if (view is androidx.recyclerview.widget.RecyclerView) {
@@ -80,19 +109,18 @@ class StatisticsListFragment : androidx.fragment.app.Fragment() {
 
         viewModel = ViewModelProviders.of(this).get(StatisticsViewModel::class.java)
 
-        if(viewModel.monthYear == null) {
+        if (viewModel.monthYear == null) {
             viewModel.getSumsForCategories()?.observe(this,
                 Observer<List<StatisticsCategory>> { categories -> adapter.setCategories(categories) })
-        }
-        else {
+        } else {
             val s = SimpleDateFormat("MM/YYYY")
             viewModel.getSumsForMonthAndCategories()?.observe(this,
                 Observer<List<StatisticsCategory>> { values ->
                     val filteredByMonthYear = (values.filter {
                         s.format(it.date) == s.format(viewModel.monthYear)
                     })
-                    filteredByMonthYear.forEach{
-                        if(it.type == TransactionType.Spending) {
+                    filteredByMonthYear.forEach {
+                        if (it.type == TransactionType.Spending) {
                             it.amount *= -1
                         }
                     }
@@ -110,7 +138,8 @@ class StatisticsListFragment : androidx.fragment.app.Fragment() {
                         )
                     }
                     adapter.setCategories(categories)
-                })}
+                })
+        }
         // Don't forget to tell the RecyclerView how to show the items! (Linear - LinearLayoutManager, Grid - GridLayoutManager etc.)
         statlist.layoutManager = LinearLayoutManager(this.context)
     }
